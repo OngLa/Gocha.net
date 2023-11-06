d87149cadd32ae68603aa54c9e7fb5ffc40e3611
Hong <seokho.hong.dev@gmail.com>
2023-11-06 20:21:46 +0900

    fix: Conflicting 해결

--- frontend/src/AppRoute.js
+++ frontend/src/AppRoute.js
@@ -1,27 +1,24 @@
-import {
-  Navigate,
-  Route,
-  Routes,
-} from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Chatting from "./pages/Chatting";
 import Car from "./pages/Car";
+import Member from "./pages/Member";
 
 // function Approute() {} 이랑 아래 에로우 함수랑 같다.
 const AppRoute = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/*" element={<Home />} />
         <Route path="/chatting/*" element={<Chatting />} />
         {/* <Route path="/reservation/*" element={<Chatting />} /> */}
         <Route path="/car/*" element={<Car />} />
         {/* <Route path="/carinfo/*" element={<Chatting />} /> */}
         {/* <Route path="/parts/*" element={<Chatting />} /> */}
+        <Route path="/member/*" element={<Member />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </>
-   
   );
 };
 

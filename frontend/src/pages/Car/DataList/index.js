722de4220ca950eff8ba2fdcf6a8dbbfa39857cc
Hong <seokho.hong.dev@gmail.com>
2023-11-08 20:30:37 +0900

    fix : 데이터관리 페이지 수정

--- frontend/src/pages/Car/DataList/index.js
+++ frontend/src/pages/Car/DataList/index.js
@@ -4,18 +4,38 @@ import LargeButton, { SmallButton } from "../../../components/Button";
 import { useRef, useState } from "react";
 import DateRangeForm from "../../../components/DateRangeForm";
 
-import style from "./style.module.css";
+import styles from "./style.module.css";
+import CustomSelect from "../../../components/CustomSelect";
 
 function DataList() {
   const [cards, setCards] = useState([
     { date: "2023-11-08", content: "차량 데이터1" },
     { date: "2023-11-09", content: "차량 데이터2" },
   ]);
+  const myCarList = [
+    {
+      value: 0,
+      name: "Sonata",
+    },
+    {
+      value: 1,
+      name: "Avante",
+    },
+    {
+      value: 2,
+      name: "Tucson",
+    },
+    {
+      value: 3,
+      name: "Santa fe",
+    },
+  ];
   const [card, setCard] = useState({
     date: "",
     content: "",
   });
   const [id, setId] = useState(3);
+  const [value, setValue] = useState(0);
 
   const dateRangeRef = useRef();
 
@@ -46,14 +66,14 @@ function DataList() {
   };
 
   return (
-    <div className={style.wrapper}>
+    <div className={styles.wrapper}>
       {/* Content의 헤더, 뒤로가기 기능 */}
-      <div className={style.menu}>
+      <div className={styles.menu}>
         <ContentHeader menuName="데이터 관리" />
       </div>
 
       {/* 최신 데이터 업데이트 버튼, 버튼 클릭 시 새로운 데이터 추가 */}
-      <div className={style.addCardBtn}>
+      <div className={styles.addCardBtn}>
         <LargeButton
           name="update"
           children="최신 데이터 업데이트"
@@ -61,9 +81,14 @@ function DataList() {
         />
       </div>
 
+      {/* 내 차량 종류 Selector */}
+      <div className={styles.customSelect}>
+        <CustomSelect items={myCarList} value={value} setValue={setValue} />
+      </div>
+
       {/* 날짜 필터 폼 */}
-      <div className={style.dateFormWrapper}>
-        <div className={style.dateForm}>
+      <div className={styles.dateFormWrapper}>
+        <div className={styles.dateForm}>
           <DateRangeForm title="검색 날짜" ref={dateRangeRef} />
         </div>
         <SmallButton
@@ -74,9 +99,9 @@ function DataList() {
       </div>
 
       {/* Card 리스트 출력 */}
-      <div className={style.cardList}>
+      <div className={styles.cardList}>
         {cards.map((item) => (
-          <div className={style.card}>
+          <div className={styles.card}>
             <Card
               key={item.id}
               title_children={

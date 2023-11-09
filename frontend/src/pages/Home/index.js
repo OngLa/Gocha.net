9d2408bb34f37a198e865c9201d13fc5735a835d
Jyassmin <88031549+Jyassmin@users.noreply.github.com>
2023-11-09 16:50:49 +0900

    Ing: fe_jm_opendata

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,12 +1,24 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import style from "./home.module.css";
 import homeBackground from "../../img/homeBackground.png";
 
-function HomeUser() {
+function Home() {
+  const [isHovered, setIsHovered] = useState(false);
+
+  useEffect(() => {
+    // Set interval to toggle isHovered every second
+    const intervalId = setInterval(() => {
+      setIsHovered((prevHovered) => !prevHovered);
+    }, 5000);
+
+    // Clean up the interval when the component is unmounted
+    return () => clearInterval(intervalId);
+  }, []); // Empty dependency array ensures that this effect runs only once when the component mounts
 
   return (
     <div
-      className={style.homeWrap}>
+      className={`${style.homeWrap} ${isHovered ? style.hovered : ""}`}
+    >
       <img
         src={homeBackground}
         alt="homeBackground"
@@ -16,4 +28,4 @@ function HomeUser() {
   );
 }
 
-export default HomeUser;
+export default Home;

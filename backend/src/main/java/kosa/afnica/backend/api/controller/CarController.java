da9cd68f95b6dc64d79fda51a858e1184808ae89
Hong <seokho.hong.dev@gmail.com>
2023-11-14 18:14:37 +0900

    feat : 차량 선택 기능 개발

--- backend/src/main/java/kosa/afnica/backend/api/controller/CarController.java
+++ backend/src/main/java/kosa/afnica/backend/api/controller/CarController.java
@@ -8,7 +8,9 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import kosa.afnica.backend.api.service.CarService;
 import kosa.afnica.backend.db.dto.car.BrandResDto;
+import kosa.afnica.backend.db.dto.car.CarTypeResDto;
 import kosa.afnica.backend.db.entity.Brand;
+import kosa.afnica.backend.db.entity.CarType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
@@ -27,26 +29,46 @@ public class CarController {
 
     private final CarService carService;
 
-
     @Operation(summary = "Brand 등록 API", description = "브랜드 등록 API - 홈페이지에서 보여주는 기능은 없음, 포스트맨으로 등록")
     @PostMapping(value = "/brand", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<Void> insertBrand(
+    public ResponseEntity<Void> postBrand(
             @RequestPart MultipartFile file,
             @RequestPart Brand brand
     ) {
-        carService.insertBrand(file, brand);
+        carService.createBrand(file, brand);
 
         return ResponseEntity.ok().build();
     }
-
-    @GetMapping("/brand")
     @Operation(summary = "Brand 불러오기 API", description = "브랜드 불러오기 API - 내 차량 등록 페이지 랜더링 시에 실행되는 API")
     @ApiResponses({
             @ApiResponse(responseCode = "200", description = "성공",
-                    content = @Content(array = @ArraySchema(schema = @Schema(implementation = Brand.class))))
+                    content = @Content(array = @ArraySchema(schema = @Schema(implementation = BrandResDto.class))))
     })
+    @GetMapping("/brand")
     public ResponseEntity<List<BrandResDto>> getBrandList() {
-        List<BrandResDto> resDtoList = carService.getBrandList();
+        List<BrandResDto> resDtoList = carService.findAllBrand();
+
+        return ResponseEntity.ok(resDtoList);
+    }
+
+    @Operation(summary = "CarTyoe 등록 API", description = "차 종류 등록 API - 홈페이지에서 보여주는 기능은 없음, 포스트맨으로 등록")
+    @PostMapping(value = "/car-type", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<Void> postCarType(
+            @RequestPart MultipartFile file,
+            @RequestPart CarType carType
+    ) {
+        carService.createCarType(file, carType);
+
+        return ResponseEntity.ok().build();
+    }
+    @Operation(summary = "CarTyoe 불러오기 API", description = "차 종류 불러오기 API - 내 차량 등록 페이지 랜더링 시에 실행되는 API")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "성공",
+                    content = @Content(array = @ArraySchema(schema = @Schema(implementation = CarTypeResDto.class))))
+    })
+    @GetMapping("/car-type")
+    public ResponseEntity<List<CarTypeResDto>> getCarTypeList(@RequestParam Long brandId) {
+        List<CarTypeResDto> resDtoList = carService.findAllCarTypeByBrandId(brandId);
 
         return ResponseEntity.ok(resDtoList);
     }
--- backend/src/main/java/kosa/afnica/backend/api/service/impl/CarServiceImpl.java
+++ backend/src/main/java/kosa/afnica/backend/api/service/impl/CarServiceImpl.java
@@ -2,7 +2,9 @@ package kosa.afnica.backend.api.service.impl;
 
 import kosa.afnica.backend.api.service.CarService;
 import kosa.afnica.backend.db.dto.car.BrandResDto;
+import kosa.afnica.backend.db.dto.car.CarTypeResDto;
 import kosa.afnica.backend.db.entity.Brand;
+import kosa.afnica.backend.db.entity.CarType;
 import kosa.afnica.backend.db.mapper.CarMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -23,8 +25,7 @@ public class CarServiceImpl implements CarService {
     private final CarMapper carMapper;
 
     @Override
-    public void insertBrand(MultipartFile file, Brand brand) {
-
+    public void createBrand(MultipartFile file, Brand brand) {
         // DB에 저장하기 위해 MultipartFile을 byte[]로 자료형 변경
         try {
             brand.setLogo(file.getBytes());
@@ -33,19 +34,45 @@ public class CarServiceImpl implements CarService {
         }
 
         // DB에 저장
-        carMapper.insertBrand(brand);
+        carMapper.saveBrand(brand);
     }
 
     @Override
-    public List<BrandResDto> getBrandList() {
-
+    public List<BrandResDto> findAllBrand() {
         // DB에서 Brand List 조회
-        List<Brand> brandList = carMapper.getBrandList();
+        List<Brand> brandList = carMapper.findAllBrand();
 
         // Brand List를 Dto로 담기
         List<BrandResDto> resDtoList = new ArrayList<>();
         for (Brand brand : brandList) {
             resDtoList.add(new BrandResDto(brand));
+        }
+
+        return resDtoList;
+    }
+
+    @Override
+    public void createCarType(MultipartFile file, CarType carType) {
+        // DB에 저장하기 위해 MultipartFile을 byte[]로 자료형 변경
+        try {
+            carType.setLogo(file.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // DB에 저장
+        carMapper.saveCarType(carType);
+    }
+
+    @Override
+    public List<CarTypeResDto> findAllCarTypeByBrandId(Long brandId) {
+        // DB에서 CarType List 조회
+        List<CarType> carTypeList = carMapper.findAllCarTypeByBrandId(brandId);
+
+        // CarType List를 Dto로 담기
+        List<CarTypeResDto> resDtoList = new ArrayList<>();
+        for (CarType carType : carTypeList) {
+            resDtoList.add(new CarTypeResDto(carType));
         }
 
         return resDtoList;

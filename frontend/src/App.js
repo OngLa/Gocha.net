35563dc5cb410106f4a4e622f96b95f0752b6533
Jyassmin <88031549+Jyassmin@users.noreply.github.com>
2023-11-14 19:12:44 +0900

    Ing: fe_jm_login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,35 @@
-import './App.css';
-import AppHeader from './components/HeaderNav/AppHeader';
-import AppRoute from './AppRoute';
-import AppNav from './components/HeaderNav/AppNav';
-import AppNavHome2 from './components/HeaderNav/AppNavHome2';
+import "./App.css";
+import AppHeader from "./components/HeaderNav/AppHeader";
+import AppRoute from "./AppRoute";
+import AppNav from "./components/HeaderNav/AppNav";
+import AppNavHome2 from "./components/HeaderNav/AppNavHome2";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
 
 function App() {
+  const gRole = useSelector((state) => state.authReducer.role);
+
+  useEffect(() => {
+    console.log("role: " + gRole);
+  });
+
   return (
     <div className="App">
       <div>
         <AppHeader />
       </div>
-      <div className='AppRouteWrap'>
+      <div className="AppRouteWrap">
         <AppRoute />
       </div>
-      <div>
-        <AppNav />
-      </div>
-      {/* 정비소Home의 Nav */}
-      {/* <div>
-        <AppNavHome2 />
-      </div> */}
+      {gRole !== "ROLE_CARCENTER" ? (
+        <div>
+          <AppNav />
+        </div>
+      ) : (
+        <div>
+          <AppNavHome2 />
+        </div>
+      )}
     </div>
   );
 }

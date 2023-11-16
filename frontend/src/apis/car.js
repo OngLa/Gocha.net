b6a8e20c038bb84db1ba3759b008be8cffb8e133
Hong <seokho.hong.dev@gmail.com>
2023-11-16 16:06:28 +0900

    feat : 내 차 등록 api 연동

--- frontend/src/apis/car.js
+++ frontend/src/apis/car.js
@@ -1,23 +1,23 @@
 import axios from "axios";
 
 export const getBrands = async () => {
-  try {
-    const response = await axios.get("http://localhost:8080/api/cars/brand");
-    // const response = await axios.get("/cars/brand");
-    return response.data;
-  } catch (error) {
-    console.error("Error get Brands, ", error);
-    throw error;
-  }
+  const response = await axios.get("http://localhost:8080/api/cars/brand");
+  // const response = await axios.get("/cars/brand");
+  return response.data;
 };
 
 export const getCarTypes = async (brandId) => {
+  const response = await axios.get("http://localhost:8080/api/cars/car-type", {
+    params: { brandId },
+  });
+  // const response = await axios.get("/cars/car-type", { params: { brandId } });
+  return response.data;
+};
+
+export const postCar = async (car) => {
   try {
-    const response = await axios.get(
-      "http://localhost:8080/api/cars/car-type",
-      { params: { brandId } }
-    );
-    // const response = await axios.get("/cars/car-type", { params: { brandId } });
+    const response = await axios.post("http://localhost:8080/api/cars", car);
+    // const response = await axios.get("/cars", car);
     return response.data;
   } catch (error) {
     console.error("Error get Brands, ", error);

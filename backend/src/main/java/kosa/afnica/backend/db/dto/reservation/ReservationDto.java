56ac74d69bf678cc9038debdf345dc113f52da2b
sihoyun <138003271+sihoyun@users.noreply.github.com>
2023-11-16 14:37:12 +0900

    Dto 순서 정렬

--- backend/src/main/java/kosa/afnica/backend/db/dto/reservation/ReservationDto.java
+++ backend/src/main/java/kosa/afnica/backend/db/dto/reservation/ReservationDto.java
@@ -9,6 +9,12 @@ import java.util.Date;
 @Getter
 @NoArgsConstructor
 public class ReservationDto {
+    private Long id;
+    private Long memberId;
+    private String state;
+    private Date reservedDate;
+    private Long carcenterId;
+
     public ReservationDto(Reservation reservation) {
         this.id = reservation.getId();
         this.memberId = reservation.getMemberId();
@@ -16,11 +22,5 @@ public class ReservationDto {
         this.reservedDate = reservation.getReservedDate();
         this.carcenterId = reservation.getCarcenterId();
     }
-
-    private Long id;
-    private Long memberId;
-    private String state;
-    private Date reservedDate;
-    private Long carcenterId;
 }
 

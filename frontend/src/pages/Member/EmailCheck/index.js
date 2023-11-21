e6b4653bf9a6efb5d3b61e3069c914498721155d
bborong <137738892+bborong@users.noreply.github.com>
2023-11-21 16:49:53 +0900

    feat : 이메일 인증 API 연동

--- frontend/src/pages/Member/EmailCheck/index.js
+++ frontend/src/pages/Member/EmailCheck/index.js
@@ -1,111 +1,131 @@
 import { useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import style from "./emailCheck.module.css";
 import LargeButton from "../../../components/Button";
 import ContentHeader from "../../../components/ContentHeader";
 import emailIcon from "../../../img/member/email.png";
 import emailCheckIcon from "../../../img/member/emailcheck.png";
 import Swal from "sweetalert2";
-import { getVeriCode } from "../../../service/member";
+import { compareVeriCode, getVeriCode } from "../../../service/member";
 
+// 이메일 인증 컴포넌트
 function EmailCheck() {
-  // 이메일 인증
+  // React Router의 navigate 훅을 사용하기 위한 초기 설정
   const navigate = useNavigate();
 
-  const [email, setEmail] = useState("");
+  // 이메일 및 인증 코드를 관리하기 위한 상태 변수
+  const [veriEmail, setVeriEmail] = useState("");
+  const [veriCode, setVeriCode] = useState("");
 
-  // const [email, setEmail] = useState({
-  //   email: "",
-  //   verificationCode: "",
-  // });
-
+  // 이메일 검증 결과 및 표시를 처리하는 상태 변수
   const [EmailCheckApplyResult, setEmailCheckApplyResult] = useState("");
   const [isEmailCheckApplied, setIsEmailCheckApplied] = useState(false);
   const [EmailCheckResult, setEmailCheckResult] = useState("");
+  const [codeMsgColor, setCodeMsgColor] = useState("");
 
-  const handleOnChange = (e) => {
-    setEmail({
-      ...email,
-      [e.target.name]: e.target.value,
-    });
-  };
+  // 이메일 및 인증 코드 필드의 입력 변경을 처리하는 이벤트 핸들러
+  const handleOnChange = useCallback((event) => {
+    const { name, value } = event.target;
 
+    // 입력 필드에 따라 상태 업데이트
+    if (name === "veriEmail") {
+      setVeriEmail(value);
+    } else if (name === "veriCode") {
+      setVeriCode(value);
+    }
+  }, []);
+
+  // 다음 버튼 클릭을 처리하는 이벤트 핸들러
   const handleOnClick = () => {
-    navigate("/member/signup");
+    // 이메일이 확인되었으며 코드 메시지 색상이 녹색인 경우
+    if (isEmailCheckApplied && codeMsgColor === "green") {
+      navigate("/member/signup", { state: { veriEmail } });
+    } else {
+      Swal.fire({
+        icon: "warning",
+        title: "인증 번호를 다시 확인해주세요.",
+        confirmButtonColor: "#45CB85",
+      });
+    }
   };
 
-  const handleEmailCheckApply = async () => {
+  // 입력된 이메일을 확인하고 인증 코드를 전송하는 이벤트 핸들러
+  const handleOnCheckEmail = async () => {
+    // 이메일 형식을 검증하는 정규식
     const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
 
     try {
-      if (email === "") {
+      // 유효성 검증 및 인증 코드 전송
+      if (veriEmail === "") {
         setEmailCheckApplyResult("이메일을 입력해주세요.");
-      } else if (!emailRegex.test(email)) {
+      } else if (!emailRegex.test(veriEmail)) {
         setEmailCheckApplyResult("올바른 이메일 형식이 아닙니다.");
-        // setIsEmailCheckApplied(false);
       } else {
-        await getVeriCode(email);
+        const requestData = { veriEmail: veriEmail };
+        const response = await getVeriCode(requestData);
+
+        // 인증 코드가 성공적으로 전송되면 성공 메시지 표시
         Swal.fire({
           icon: "success",
           title: "인증 번호가 발송되었습니다.",
           text: "인증 번호를 확인하신 후 아래 입력창에 입력해주세요.",
           confirmButtonColor: "#45CB85",
         });
+
+        // 이메일이 검증되었음을 나타내는 상태 업데이트
         setIsEmailCheckApplied(true);
+        console.log("이메일 인증 성공", response.data);
       }
-      console.log("이메일 인증 성공", email);
     } catch (error) {
+      // 이메일 검증 실패시 오류 메시지 표시
       setEmailCheckApplyResult("이미 사용 중인 이메일입니다.");
       console.log("이메일 인증 실패", error);
     }
   };
 
-  //     if (email.email === "") {
-  //     setEmailCheckApplyResult("이메일을 입력해주세요.");
-  //   } else {
-  //     if (!emailRegex.test(email.email) === true) {
-  //       setEmailCheckApplyResult("올바른 이메일 형식이 아닙니다.");
-  //       setIsEmailCheckApplied(false);
-  //     } else if(){
-  //       Swal.fire({
-  //         icon: "success",
-  //         title: "인증 번호가 발송되었습니다.",
-  //         text: "인증 번호를 확인하신 후 아래 입력창에 입력해주세요.",
-  //         confirmButtonColor: "#45CB85",
-  //       });
-  //       setIsEmailCheckApplied(true);
-  //     }
-  //   }
-  // };
-
-  const handleEmailCheckNum = () => {
-    setEmailCheckResult("인증 번호가 일치하지 않습니다.");
+  // 입력된 인증 코드를 확인하는 이벤트 핸들러
+  const handleOnCheckCode = async () => {
+    try {
+      // 입력된 인증 코드를 비교
+      await compareVeriCode(veriEmail, veriCode);
+      setEmailCheckResult("인증 번호가 일치합니다.");
+      setCodeMsgColor("green");
+    } catch (error) {
+      // 인증 코드가 일치하지 않으면 오류 메시지 표시
+      setEmailCheckResult("인증 번호가 일치하지 않습니다.");
+      setCodeMsgColor("red");
+    }
   };
 
   return (
     <div>
+      {/* 이메일 확인 섹션을 위한 헤더를 표시 */}
       <div className={style.menu}>
         <ContentHeader menuName="이메일 인증" />
       </div>
+      {/* 이메일 확인 양식 */}
       <div className={style.emailWrap}>
+        {/* 이메일을 입력하는 입력 필드 */}
         <div className={style.emailBox}>
           <img src={emailIcon} alt="emailIcon" className={style.emailImg} />
           <input
             className={style.emailInput}
             type="email"
             placeholder="이메일"
-            name="email"
-            value={email}
-            onChange={(e) => {
-              handleOnChange(e);
-            }}
+            name="veriEmail"
+            value={veriEmail}
+            onChange={handleOnChange}
           />
         </div>
+        {/* 이메일 검증 결과를 표시 */}
         <div className={style.result}>{EmailCheckApplyResult}</div>
+        {/* 이메일을 확인하고 인증 코드를 전송하는 버튼 */}
         <div className={style.EmailCheckApplyBtn}>
-          <LargeButton onClick={handleEmailCheckApply}>인증요청</LargeButton>
+          <LargeButton onClick={handleOnCheckEmail}>인증요청</LargeButton>
         </div>
+        {/* 이메일이 검증되면 인증 코드 입력 필드를 표시 */}
         {isEmailCheckApplied && (
+          //  {/* 인증번호를 입력하는 입력 필드 */}
           <div className={style.emailBox}>
             <img
               src={emailCheckIcon}
@@ -115,24 +135,30 @@ function EmailCheck() {
             <input
               className={style.emailInput}
               type="text"
-              placeholder="인증번호 입력"
-              name="verificationCode"
-              onChange={(e) => {
-                handleOnChange(e);
-              }}
+              placeholder="인증 번호 입력"
+              name="veriCode"
+              value={veriCode}
+              onChange={handleOnChange}
             />
+            {/* 입력된 인증 코드를 확인하는 버튼 */}
             <button
               className={style.EmailCheckNumBtn}
-              onClick={handleEmailCheckNum}
+              onClick={handleOnCheckCode}
             >
               확인
             </button>
           </div>
         )}
-        <div className={style.result}>{EmailCheckResult}</div>
-        <div className={style.nextBtn}>
-          <LargeButton onClick={handleOnClick}>다음</LargeButton>
+        {/* 인증 코드 확인 결과를 표시 */}
+        <div style={{ color: codeMsgColor, textAlign: "center" }}>
+          {EmailCheckResult}
         </div>
+        {/* 다음 단계로 진행하는 버튼을 표시 */}
+        {isEmailCheckApplied && (
+          <div className={style.nextBtn}>
+            <LargeButton onClick={handleOnClick}>다음</LargeButton>
+          </div>
+        )}
       </div>
     </div>
   );
--- frontend/src/service/member.js
+++ frontend/src/service/member.js
@@ -2,19 +2,19 @@ import axios from "axios";
 import qs from "qs";
 
 // 이메일 인증번호 요청
-export function getVeriCode(email) {
-  return axios.get("/member/email", { params: { email } });
+export function getVeriCode(veriEmail) {
+  return axios.post("/member/email", veriEmail);
 }
 
 // 이메일 인증번호 확인
-export function postVeriCode(verification) {
-  return axios.get("/member/email-veri", verification);
+export function compareVeriCode(veriEmail, veriCode) {
+  return axios.get("/member/email-veri", { params: { veriEmail, veriCode } });
 }
 
 // 닉네임 중복
 export function nameCheck(nickname) {
   return axios.get("member/name-check", { params: { nickname } });
-}  
+}
 
 // 회원가입
 export function createMember(member) {
@@ -23,5 +23,5 @@ export function createMember(member) {
 
 // 마이페이지
 export function getMypage(email) {
-  return axios.get("/member/mypage")
+  return axios.get("/member/mypage");
 }

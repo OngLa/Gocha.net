ab0e010b8bf9ebd62fb714aa2694ce6ac85b94d2
Jyassmin <88031549+Jyassmin@users.noreply.github.com>
2023-11-21 20:55:11 +0900

    Revert "Be jm chattinglist"

--- backend/src/main/java/kosa/afnica/backend/api/service/ReservationService.java
+++ backend/src/main/java/kosa/afnica/backend/api/service/ReservationService.java
@@ -1,6 +1,5 @@
 package kosa.afnica.backend.api.service;
 
-import kosa.afnica.backend.db.dto.reservation.ReservationResDto;
 import kosa.afnica.backend.db.dto.reservation.AdminDto;
 import kosa.afnica.backend.db.dto.reservation.ReservationDto;
 import kosa.afnica.backend.db.dto.reservation.ReservationReqDto;
@@ -10,8 +9,6 @@ import java.util.List;
 
 public interface ReservationService {
 
-     public List<ReservationResDto> readReservationList(HttpServletRequest request);
-     public void createReservation(HttpServletRequest request, ReservationReqDto reservationReqDto);
     //     예약 출력
     public List<ReservationDto> findReservationList(HttpServletRequest request);
 

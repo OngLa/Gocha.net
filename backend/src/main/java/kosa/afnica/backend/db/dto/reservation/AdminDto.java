3f99f7097800703639a7c2a7fc700e0326e1fee8
sihoyun <138003271+sihoyun@users.noreply.github.com>
2023-11-23 19:47:07 +0900

    날짜 형식 변경

--- backend/src/main/java/kosa/afnica/backend/db/dto/reservation/AdminDto.java
+++ backend/src/main/java/kosa/afnica/backend/db/dto/reservation/AdminDto.java
@@ -1,9 +1,10 @@
 package kosa.afnica.backend.db.dto.reservation;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor
@@ -11,14 +12,15 @@ public class AdminDto {
 
     private Long id;
     private Long state;
-    private Date reservedDate;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
+    private LocalDateTime reservedDate;
     private Long memberId;
     private Long carcenterId;
     private Long carDataId;
     private String phoneNumber;
     private String name;
 
-    public AdminDto(Long id, Long state, Date reservedDate, Long memberId, Long carcenterId, Long carDataId, String phoneNumber, String name) {
+    public AdminDto(Long id, Long state, LocalDateTime reservedDate, Long memberId, Long carcenterId, Long carDataId, String phoneNumber, String name) {
         this.id = id;
         this.state = state;
         this.reservedDate = reservedDate;

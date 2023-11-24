69494910b3286978984b3c5630d004f66ca8b0f4
sihoyun <138003271+sihoyun@users.noreply.github.com>
2023-11-24 16:54:12 +0900

    fe-ysh-상세보기

--- frontend/src/pages/Reservation/RepairshopComponent/CardHead.js
+++ frontend/src/pages/Reservation/RepairshopComponent/CardHead.js
@@ -1,13 +1,25 @@
 import styles from "./repairshopcomponent.module.css";
-import carcenterIcon from "../../../img/chatting/carcenterIcon.png"
+import carcenterIcon from "../../../img/chatting/carcenterIcon.png";
+import { useNavigate } from "react-router";
 
-function CardHead({carcenter}) {
+function CardHead({ carcenter }) {
+  const navigate = useNavigate();
+
+  const pageMoveText = () => {
+    navigate(
+      `/chatting/chatroominfo?carcenterId=${carcenter.id}&carcenterName=${carcenter.name}`
+    );
+  };
+
   return (
     <div className={styles.cardHead}>
       <div className={styles.carcenterIcon}>
-        <img src={carcenterIcon} alt="carcenterIcon"/>
+        <img src={carcenterIcon} alt="carcenterIcon" />
       </div>
       <div className={styles.carcenterName}>{carcenter.name}</div>
+      <div onClick={pageMoveText} className={styles.chatHistory}>
+        상세정보보기
+      </div>
     </div>
   );
 }

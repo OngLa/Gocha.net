9a9c4661eda425a241fa7356112c93cb1021064a
Hong <seokho.hong.dev@gmail.com>
2023-11-24 16:09:57 +0900

    fix : css수정&주석추가 1. 달력 컴포넌트 주석 추가 2. 달력 컴포넌트 날짜 선택시 달력에도 선택값 표시 3. 날짜 입력 컴포넌트 주석 추가 4. 날짜 입력에 대한 유효성 검사 로직 수정 5. 기간 검색 컴포넌트 컴포넌트 구조 변경 6. 기간 검색 컴포넌트 주석 추가

--- frontend/src/components/DateRangeForm/Calendar.js
+++ frontend/src/components/DateRangeForm/Calendar.js
@@ -1,19 +1,23 @@
+// import
+import React from "react";
 import Datetime from "react-datetime";
-import moment from 'moment';
-import 'moment/locale/ko';
+import moment from "moment";
+import "moment/locale/ko";
+import styles from "./calendar.module.css";
 
-import style from "./calendar.module.css";
+// 달력에 기본 언어를 한국어로 설정
+moment.locale("ko");
 
-// 달력 한국어로 설정
-moment.locale('ko');
-
+// Calendar Component
+// Props : date(선택된 날짜), onChange(날짜 변경 핸들러)
 function Calendar(props) {
   const { date, onChange } = props;
 
+  // 날짜 형식
   const dateFormat = "YYYY-MM-DD";
 
   return (
-    <div className={style.calWrapper}>
+    <div className={styles.calWrapper}>
       <Datetime
         value={date}
         dateFormat={dateFormat}
@@ -26,4 +30,5 @@ function Calendar(props) {
   );
 }
 
-export default Calendar;
+// Component 최적화, Props 값의 변화에 따른 리랜더링 방지
+export default React.memo(Calendar);
